d2855e9a252f5c54253e0aa48e7b355b9f1f304d
Yuce Tekol <yucetekol@gmail.com>
2020-10-02 14:41:39 +0300

    Updated libs, added more benchmarks, getting ready for release

--- README.md
+++ README.md
@@ -1,29 +1,91 @@
-# cloudevents-java-benchmarks
+# CloudEvents Java Benchmarks
+
+Benchmarks of CloudEvent implementations for Java.
 
 ## Usage
 
-### Creating a Stand-alone Package Without JDK Requirement
+Requirements:
 
-Creates a Zip archive with compiled project files, which **doesn't** require the JDK to be present.
+* JDK 8
 
-JDK 11 or higher is required to create the package.
+Run `make benchmark` on Linux, MacOS, etc. On Windows `gradlew --no-daemon jmh` should work.
 
-* Run `make package-standalone` to create the Zip archive *build/PROJECT_NAME-PROJECT_VERSION.zip*
-* When the archive is unpackaged, the `bin` directory contains the `PROJECT_NAME` executable which starts the project.
+## Benchmarks
 
-### Creating a Jar Package
+### CloudEvents SDK for Java
 
-Creates a Jar file with all dependencies. Running the Jar requires a JDK or to be present. A minimal JRE 8 for Linux can be found at: https://scaleplan.net/jre 
+Official CloudEvents SDK for Java: https://github.com/cloudevents/sdk-java
 
-JDK 8 or higher is required to create the Jar.
+## JSON
 
-* Run `make package` to create the Jar file `build/libs/PROJECT_NAME-PROJECT_VERSION-all.jar`
-* The Jar can be run using: `java -jar PROJECT_NAME-PROJECT_VERSION-all.jar` (assuming the Jar is in the current directory).
+CloudEvents 1.0 JSON spec.
 
-### Creating a Distribution Package 
+* `benchmarkSdkJsonDecodeEvent`: Decode a single event
+* `benchmarkSdkJsonEncodeEvent`: Encode a single event
 
-Creates an archive with the project and startup scripts. This way of creating a package is almost equivalent to the Jar package explained above but has shell scripts to run the Jar.
+## ScalePlan CloudEvents for Java
 
-* Run `make package-dist` to create the archive `build/distributions/PROJECT_NAME-shadow-PROJECT_VERSION-SNAPSHOT.zip
-* When the archive is unpackaged, the `bin` directory contains the `PROJECT_NAME-PROJECT_VERSION` executable which starts the project.
+SPCE CloudEvents for Java: https://github.com/scaleplandev/spce-java
 
+### JSON
+
+CloudEvents 1.0 JSON spec.
+
+* `benchmarkSpceJsonDecodeEvent`: Decode a single event
+* `benchmarkSpceJsonEncodeEvent`: Encode a single event
+* `benchmarkSpceJsonDecodeEventBundle10`: Decode a bundle of 10 events
+* `benchmarkSpceJsonEncodeEventBundle10`: Encode a bundle of 10 events
+
+### Avro
+
+Using CloudEvents 1.0 Avro schema at https://github.com/cloudevents/spec/blob/v1.0/spec.avsc
+
+* `benchmarkAvroDecodeEvent`: Decode a single event
+* `benchmarkAvroEncodeEvent`: Encode a single event
+
+### Avro SPCE
+
+Using SPCE 1.0 Avro schema at:
+
+* `benchmarkAvroSpceDecodeEvent`: Decode a single event
+* `benchmarkAvroSpceEncodeEvent`: Encode a single event
+* `benchmarkAvroSpceDecodeEventBundle10`: Decode a bundle of 10 events
+* `benchmarkAvroSpceEncodeEventBundle10`: Encode a bundle of 10 events
+
+## Sample Result
+
+Below is a result from a typical run on my system. The exact numbers may be different on your system, but the overall picture should be the same.
+
+    Benchmark                                                  Mode  Cnt    Score    Error   Units
+    EventCodecBenchmark.benchmarkAvroDecodeEvent              thrpt   10  253.527 ±  8.203  ops/ms
+    EventCodecBenchmark.benchmarkAvroEncodeEvent              thrpt   10  269.207 ±  9.189  ops/ms
+    EventCodecBenchmark.benchmarkAvroSpceDecodeEvent          thrpt   10  461.694 ± 16.964  ops/ms
+    EventCodecBenchmark.benchmarkAvroSpceDecodeEventBundle10  thrpt   10   39.816 ±  7.879  ops/ms
+    EventCodecBenchmark.benchmarkAvroSpceEncodeEvent          thrpt   10  408.037 ± 17.207  ops/ms
+    EventCodecBenchmark.benchmarkAvroSpceEncodeEventBundle10  thrpt   10   40.102 ±  3.235  ops/ms
+    EventCodecBenchmark.benchmarkSdkJsonDecodeEvent           thrpt   10  268.276 ± 16.190  ops/ms
+    EventCodecBenchmark.benchmarkSdkJsonEncodeEvent           thrpt   10  319.856 ± 12.765  ops/ms
+    EventCodecBenchmark.benchmarkSpceJsonDecodeEvent          thrpt   10  519.292 ± 19.978  ops/ms
+    EventCodecBenchmark.benchmarkSpceJsonDecodeEventBundle10  thrpt   10   50.152 ± 10.112  ops/ms
+    EventCodecBenchmark.benchmarkSpceJsonEncodeEvent          thrpt   10  942.562 ± 16.331  ops/ms
+    EventCodecBenchmark.benchmarkSpceJsonEncodeEventBundle10  thrpt   10   98.450 ±  5.817  ops/ms
+
+## License
+
+(c) 2020 Scale Plan Yazılım A.Ş. https://scaleplan.io
+
+Licensed under [Apache 2.0](LICENSE). See the [LICENSE](LICENSE).
+
+    Copyright 2020 Scale Plan Yazılım A.Ş.
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- src/jmh/java/cloudevents/benchmark/EventCodecBenchmark.java
+++ src/jmh/java/cloudevents/benchmark/EventCodecBenchmark.java
@@ -1,3 +1,17 @@
+// Copyright 2020 Scale Plan Yazılım A.Ş.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cloudevents.benchmark;
 
 import io.cloudevents.core.provider.EventFormatProvider;
@@ -8,6 +22,8 @@ import io.scaleplan.spce.codecs.Json;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.Throughput)
@@ -15,98 +31,113 @@ import java.util.concurrent.TimeUnit;
 @Fork(value = 2)
 @Warmup(iterations = 2)
 public class EventCodecBenchmark {
-    private static final byte[] text = sampleEventText();
-    //    private static final Event sampleEvent = sampleScalePlanEventWithOptionalAttributes();
-    private static final io.scaleplan.spce.CloudEvent scaleplanEvent = Json.decode(text);
-    private static final byte[] scaleplanAvroEncodedEvent = Avro.encode(scaleplanEvent);
-    private static final byte[] getScaleplanAvroAltEncodedEvent = AvroSpce.encode(scaleplanEvent);
+    private static final byte[] text = eventText();
 
     // CloudEvents SDK stuff
     private static final JsonFormat format = getFormat();
     private static final io.cloudevents.CloudEvent sdkEvent = format.deserialize(text);
 
-    @Benchmark
-    public void benchmarkScalePlanDecodeEvent(Blackhole blackhole) {
-        blackhole.consume(Json.decode(text));
-    }
+    // SPCE stuff
+    private static final io.scaleplan.spce.CloudEvent spceEvent = Json.decode(text);
+    private static final List<io.scaleplan.spce.CloudEvent> spceEventBundle10 = eventBundle(10);
 
-    /*
+    private static final byte[] spceJsonEncodedEvent = Json.encode(spceEvent);
+    private static final byte[] spceJsonEncodedEventBundle10 = Json.encode(spceEventBundle10);
+
+    private static final byte[] spceAvroEncodedEvent = Avro.encode(spceEvent);
+    private static final byte[] spceAvroSpceEncodedEvent = AvroSpce.encode(spceEvent);
+    private static final byte[] spceAvroSpceEncodedEventBundle10 = AvroSpce.encode(spceEventBundle10);
+
+    // CloudEvents SDK JSON Benchmarks
+
     @Benchmark
-    public void benchmarkCloudEventsSdkDecodeEvent(Blackhole blackhole) {
+    public void benchmarkSdkJsonDecodeEvent(Blackhole blackhole) {
         blackhole.consume(format.deserialize(text));
     }
-     */
+
+    @Benchmark
+    public void benchmarkSdkJsonEncodeEvent(Blackhole blackhole) {
+        blackhole.consume(format.withForceJsonDataToBase64().serialize(sdkEvent));
+    }
+
+    // SPCE JSON Benchmarks
+
+    @Benchmark
+    public void benchmarkSpceJsonDecodeEvent(Blackhole blackhole) {
+        blackhole.consume(Json.decode(spceJsonEncodedEvent));
+    }
+
+    @Benchmark
+    public void benchmarkSpceJsonEncodeEvent(Blackhole blackhole) {
+        blackhole.consume(Json.encode(spceEvent));
+    }
+
+    @Benchmark
+    public void benchmarkSpceJsonDecodeEventBundle10(Blackhole blackhole) {
+        blackhole.consume(Json.decodeBatch(spceJsonEncodedEventBundle10));
+    }
+
+    @Benchmark
+    public void benchmarkSpceJsonEncodeEventBundle10(Blackhole blackhole) {
+        blackhole.consume(Json.encode(spceEventBundle10));
+    }
+
+    // SPCE Avro Benchmarks (CloudEvents Avro schema)
 
     @Benchmark
     public void benchmarkAvroDecodeEvent(Blackhole blackhole) {
-        blackhole.consume(Avro.decode(scaleplanAvroEncodedEvent));
-    }
-
-    @Benchmark
-    public void benchmarkAvroAltDecodeEvent(Blackhole blackhole) {
-        blackhole.consume(AvroSpce.decode(getScaleplanAvroAltEncodedEvent));
-    }
-
-    @Benchmark
-    public void benchmarkScalePlanEncodeEvent(Blackhole blackhole) {
-        blackhole.consume(Json.encode(scaleplanEvent));
-    }
-
-    @Benchmark
-    public void benchmarkCloudEventsSdkEncodeEvent(Blackhole blackhole) {
-        blackhole.consume(format.withForceJsonDataToBase64().serialize(sdkEvent));
+        blackhole.consume(Avro.decode(spceAvroEncodedEvent));
     }
 
     @Benchmark
     public void benchmarkAvroEncodeEvent(Blackhole blackhole) {
-        blackhole.consume(Avro.encode(scaleplanEvent));
+        blackhole.consume(Avro.encode(spceEvent));
+    }
+
+    // SPCE AvroSpce Benchmarks (SPCE Avro schema)
+
+    @Benchmark
+    public void benchmarkAvroSpceDecodeEvent(Blackhole blackhole) {
+        blackhole.consume(AvroSpce.decode(spceAvroSpceEncodedEvent));
     }
 
     @Benchmark
-    public void benchmarkAvroAltEncodeEvent(Blackhole blackhole) {
-        blackhole.consume(AvroSpce.encode(scaleplanEvent));
+    public void benchmarkAvroSpceEncodeEvent(Blackhole blackhole) {
+        blackhole.consume(AvroSpce.encode(spceEvent));
     }
 
-    /*
-    private static Event sampleEventWsampleEventWithRequiredAttributes() {
-        return MutableEventImpl.create(
-                "OximeterMeasured",
-                "/user/123#",
-                "567"
-        );
+    @Benchmark
+    public void benchmarkAvroSpceDecodeEventBundle10(Blackhole blackhole) {
+        blackhole.consume(AvroSpce.decodeBatch(spceAvroSpceEncodedEventBundle10));
     }
 
-    private static Event sampleScalePlanEventWithOptionalAttributes() {
-        MutableEvent event = (MutableEvent)sampleEventWithRequiredAttributes();
-        event
-                .setTime("2020-07-13T09:15:12Z")
-                .setDataContentType("application/json")
-                .setData(sampleData())
-                .setDataSchema("http://json-schema.org/draft-07/schema#")
-                .setSubject("SampleSubject");
-        return event;
+    @Benchmark
+    public void benchmarkAvroSpceEncodeEventBundle10(Blackhole blackhole) {
+        blackhole.consume(AvroSpce.encode(spceEventBundle10));
     }
 
-    private static byte[] sampleData() {
-        String text = "{\"user_id\": \"bc1459c5-378d-4835-b4b6-a2c7d9ca75e3\", \"spo2\": 96, \"event\": \"OximiterMeasured\"}";
-        return text.getBytes(StandardCharsets.UTF_8);
-    }
-     */
+    // Utility
 
-    private static byte[] sampleEventText() {
+    private static byte[] eventText() {
         String text = "{\n" +
                 "  \"specversion\": \"1.0\",\n" +
                 "  \"id\": \"1\",\n" +
                 "  \"type\": \"mock.test\",\n" +
                 "  \"source\": \"http://localhost/source\",\n" +
                 "  \"dataschema\": \"http://localhost/schema\",\n" +
-                "  \"datacontenttype\": \"application/json\",\n" +
+                "  \"datacontenttype\": \"application/octet-stream\",\n" +
                 "  \"data_base64\": \"e30=\",\n" +
                 "  \"subject\": \"sub\",\n" +
                 "  \"external1\": \"foobar\",\n" +
                 "  \"time\": \"2018-04-26T14:48:09+02:00\"\n" +
                 "}";
         return text.getBytes();
+    }
+
+    private static List<io.scaleplan.spce.CloudEvent> eventBundle(int n) {
+        List<io.scaleplan.spce.CloudEvent> bundle = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) bundle.add(spceEvent);
+        return bundle;
     }
 
     private static JsonFormat getFormat() {
